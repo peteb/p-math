f0305c94c58c9ce1b2f3ed5ff3b2825068581ca4
Peter Backman <ptr.bckmn@gmail.com>
2011-06-11 11:11:19 +0200

    restructured utils a bit

--- include/utils/algorithm.h
+++ include/utils/algorithm.h
@@ -6,19 +6,25 @@
 namespace p {
 
 template<typename T, typename Scalar>
-T lerp(const T &begin, const T &end, Scalar amount) {
+T lerp(T begin, T end, Scalar amount) {
   return begin + (end - begin) * amount;
 }
 
 
-template<typename ValueT>
-ValueT clamp(const ValueT &val, const ValueT &min, const ValueT &max) {
+template<typename T>
+T clamp(T val, T min, T max) {
   return std::max(std::min(val, max), min);
 }
 
-inline double wrap(double value, double lower, double upper) {
-  double distance = upper - lower;
-  double times = floor((value - lower) / distance);
+template<typename T>
+T saturate(T val) {
+  return clamp(val, static_cast<T>(0.0), static_cast<T>(1.0));
+}
+
+template<typename T>
+inline T wrap(T value, T lower, T upper) {
+  T distance = upper - lower;
+  T times = floor((value - lower) / distance);
   return value - (times * distance);
 }
 
