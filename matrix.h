d169660f06d67171d336c7804c8d1c31edf9700d
Peter Backman <ptr.bckmn@gmail.com>
2012-01-01 15:47:43 +0100

    ns indent for matrix also

--- matrix.h
+++ matrix.h
@@ -9,40 +9,40 @@
 
 namespace p {
 
-template<typename T, std::size_t N>
-class vec;
+  template<typename T, std::size_t N>
+  class vec;
 
-/**
- * The general case.
- */
-template<typename T, std::size_t M, std::size_t N>
-struct mat {
-  typedef T value_type;
-  enum {width = M};
-  enum {height = N};
+  /**
+   * The general case.
+   */
+  template<typename T, std::size_t M, std::size_t N>
+  struct mat {
+    typedef T value_type;
+    enum {width = M};
+    enum {height = N};
 
-  explicit mat() {}
-  explicit mat(T val) {
-    std::uninitialized_fill(components, components + M*N, val);
-  }
+    explicit mat() {}
+    explicit mat(T val) {
+      std::uninitialized_fill(components, components + M*N, val);
+    }
 
-  vec<T, M> &row(std::size_t j) {
-    return reinterpret_cast<vec<T, M> &>(components[M*j]);
-  }
+    vec<T, M> &row(std::size_t j) {
+      return reinterpret_cast<vec<T, M> &>(components[M*j]);
+    }
 
-  const vec<T, M> &row(std::size_t j) const {
-    return reinterpret_cast<vec<T, M> &>(components[M*j]);
-  }
+    const vec<T, M> &row(std::size_t j) const {
+      return reinterpret_cast<vec<T, M> &>(components[M*j]);
+    }
   
-  T components[M*N];
-};
+    T components[M*N];
+  };
 
 
-template<typename T, std::size_t M> struct mat<T, M, 0> {private: mat(); };
-template<typename T, std::size_t N> struct mat<T, 0, N> {private: mat(); };
+  template<typename T, std::size_t M> struct mat<T, M, 0> {private: mat(); };
+  template<typename T, std::size_t N> struct mat<T, 0, N> {private: mat(); };
 
-typedef mat<float, 3, 3> mat3;
-typedef mat<float, 4, 4> mat4;
+  typedef mat<float, 3, 3> mat3;
+  typedef mat<float, 4, 4> mat4;
 }
 
 #endif // !P_UTILS_MATRIX_H
