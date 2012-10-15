adeb2e8ba4c10c4d756e07d1e04230d868343baf
Peter Backman <ptr.bckmn@gmail.com>
2012-10-15 16:32:52 +0200

    ouch, tabs

--- vector_stream.h
+++ vector_stream.h
@@ -32,6 +32,7 @@
 #include <limits>
 #include <sstream>
 #include <locale>
+#include <map>
 
 #include "vector.h"
 
@@ -106,7 +107,7 @@ namespace p {
     template<typename T, std::size_t size>
     bool color_parse_hex(const std::string &str, vec<T, size> &ret) {
       if (str.size() <= 2 || str[0] != '0' || str[1] != 'x')
-	return false;
+        return false;
       
       // parse it as hex
       // convert_from_bin_component to type_component
@@ -116,8 +117,8 @@ namespace p {
       //       fast-path for those.
       unsigned long val = std::strtoul(str.c_str(), NULL, 16);
       for (std::size_t i = size; i--; ) {
-	ret[i] = (val & 0xFF) * (color_limits<T>::max() / 255.0);
-	val >>= 8;
+        ret[i] = (val & 0xFF) * (color_limits<T>::max() / 255.0);
+        val >>= 8;
       }
 
       return true;
@@ -125,82 +126,92 @@ namespace p {
     
     template<typename T>
     class textual_color_parser<T, 3> {
+      std::map<std::string, vec<T, 3>> presets;
+
+      typedef color_limits<T> lims;
+      
     public:
+      textual_color_parser() :
+        presets{
+        "red", {lims::max(), T(), T()}}
+    }
+      {}
+      
       bool get(const char *str, vec<T, 3> &retval) const {
-	std::string lower(str);
-	std::transform(begin(lower), end(lower),
-		       begin(lower), [](char &c){return std::tolower(c);});
-	// we include both cctype and locale, so we need a lambda.
-	
-	if (lower == "red") {
-	  retval = vec<T, 3>{color_limits<T>::max(), T(), T()};
-	}
-	else {
-	  return color_parse_hex(lower, retval);
-	}
-	  
-	return true;
-      }
-    };
-
-    // this class is for parsing alpha, it uses the 3 component parser
-    // but sets the last component to max.
-    template<typename T>
-    class textual_color_parser<T, 4> {
-      textual_color_parser<T, 3> subparser;
-      
-    public:
-      bool get(const char *str, vec<T, 4> &retval,
-	       const T &alpha = color_limits<T>::max()) const {
-	if (subparser.get(str, truncate(retval))) {
-	  retval[3] = alpha;
-	  return true;
-	}
-
-	return false;
-      }
-    };
-	
-    /**
-     * Parsing helping class for reading colors from stream.
-     * Should be created with p::color_reader.
-     */
-    template<typename T, std::size_t size>
-    class color_reader_impl {
-    public:
-      color_reader_impl(vec<T, size> &target)
-        : target(target)
-      {
-      }
-      
-      template<typename Stream>
-      Stream &read(Stream &is) const {
-        const streamstate<Stream> start(is);
-        if (!start)
-          return is;
-        
-        if (!(is >> target)) {
-          start.reset(is);
-          
-          std::string textual;
-          if (is >> textual) {
-            // TODO: test with whitespace
-            // TODO: first try parsing as components
-
-	    static textual_color_parser<T, size> parser;
-	    if (!parser.get(textual.c_str(), target))
-	      fail(is);
-          }
+        std::string lower(str);
+        std::transform(begin(lower), end(lower),
+                       begin(lower), [](char &c){return std::tolower(c);});
+        // we include both cctype and locale, so we need a lambda.
+        auto iter = presets.find(lower);
+        if (iter != end(presets)) {
+          retval = iter->second;
         }
-        
-        return is;
+        else {
+          return color_parse_hex(lower, retval);
+        }
+	  
+        return true;
       }
-      
-    private:
-      vec<T, size> &target;
-    };
+  };
 
-  } // !detail
+  // this class is for parsing alpha, it uses the 3 component parser
+  // but sets the last component to max.
+  template<typename T>
+  class textual_color_parser<T, 4> {
+    textual_color_parser<T, 3> subparser;
+      
+  public:
+    bool get(const char *str, vec<T, 4> &retval,
+             const T &alpha = color_limits<T>::max()) const {
+      if (subparser.get(str, truncate(retval))) {
+        retval[3] = alpha;
+        return true;
+      }
+
+      return false;
+    }
+  };
+	
+  /**
+   * Parsing helping class for reading colors from stream.
+   * Should be created with p::color_reader.
+   */
+  template<typename T, std::size_t size>
+  class color_reader_impl {
+  public:
+    color_reader_impl(vec<T, size> &target)
+      : target(target)
+    {
+    }
+      
+    template<typename Stream>
+    Stream &read(Stream &is) const {
+      const streamstate<Stream> start(is);
+      if (!start)
+        return is;
+        
+      if (!(is >> target)) {
+        start.reset(is);
+          
+        std::string textual;
+        if (is >> textual) {
+          // TODO: test with whitespace
+          // TODO: first try parsing as components
+
+          static textual_color_parser<T, size> parser;
+          if (!parser.get(textual.c_str(), target))
+            fail(is);
+        }
+      }
+        
+      return is;
+    }
+      
+  private:
+    vec<T, size> &target;
+  };
+
+} // !detail
 
 
   /**
@@ -212,91 +223,91 @@ namespace p {
    * vec3 color;
    * std::cin >> p::color_reader(color);
    */
-  template<typename T, std::size_t size>
-  detail::color_reader_impl<T, size> color_reader(vec<T, size> &target) {
-    return detail::color_reader_impl<T, size>(target);
-  }
+template<typename T, std::size_t size>
+detail::color_reader_impl<T, size> color_reader(vec<T, size> &target) {
+  return detail::color_reader_impl<T, size>(target);
+}
 
-  // TODO: color_writer
+// TODO: color_writer
   
-  /**
-   * Output a vector in the format x[ y[ z[...]]] unless std::hex has
-   * been set on the stream.
-   * For example, "123, 43".
-   */
-  template<typename T, std::size_t size>
-  std::ostream &operator <<(std::ostream &s, const vec<T, size> &v) {
-    const std::ostream::sentry sentry(s);
-    if (!sentry)
-      return s;
-    
-    if (s.flags() & std::ios::hex) {
-      // TODO: this shouldn't be here... make a color_writer
-      std::stringstream ss;
-      ss << "0x" << std::hex;
-      for (std::size_t i = size; i--; ) {
-        double fraction = v[i] / double(detail::color_limits<T>::max());
-        ss << int(fraction * 255.0);
-      }
-      
-      // this safety is probably unnecessary.
-      s << (ss ? ss : detail::fail(s));
-    }
-    else {
-      typedef typename detail::int_type<T>::type text_rep;
-      
-      s << text_rep(v[0]);
-      for (std::size_t i = 1; i < size; ++i)
-        s << " " << text_rep(v[i]);
-    }
-  
-    return s;  
-  }  
-
-  /**
-   * Read a generic vector from a stream. Also supports
-   * special values 'null'/'zero' for a vector with 0 length.
-   */
-  template<typename T, std::size_t size, typename InStream>
-  InStream &operator >>(InStream &s, vec<T, size> &v) {
-    const detail::streamstate<InStream> start(s);
-    if (!start)
-      return s;
-    
-    if (s >> v.components[0]) {
-      for (std::size_t i = 1; i < size && s.good(); ++i) {
-        typename detail::int_type<T>::type tmp;
-        s >> tmp;
-        v[i] = tmp;
-      }
-    }
-    else {
-      // it seems we can't parse it as a native type of the vector,
-      // so we parse it as a string.
-      start.reset(s);
-      std::string textual;
-      
-      if (s >> textual) {
-        using detail::stricmp;
-        if (stricmp(textual.c_str(), "null") ||
-            stricmp(textual.c_str(), "zero")) {
-          for (std::size_t i = 0; i < size; ++i)
-            v[i] = T();
-        }
-        else {
-          s.clear(std::ios::failbit);
-        }
-      }
-    }
-  
+/**
+ * Output a vector in the format x[ y[ z[...]]] unless std::hex has
+ * been set on the stream.
+ * For example, "123, 43".
+ */
+template<typename T, std::size_t size>
+std::ostream &operator <<(std::ostream &s, const vec<T, size> &v) {
+  const std::ostream::sentry sentry(s);
+  if (!sentry)
     return s;
+    
+  if (s.flags() & std::ios::hex) {
+    // TODO: this shouldn't be here... make a color_writer
+    std::stringstream ss;
+    ss << "0x" << std::hex;
+    for (std::size_t i = size; i--; ) {
+      double fraction = v[i] / double(detail::color_limits<T>::max());
+      ss << int(fraction * 255.0);
+    }
+      
+    // this safety is probably unnecessary.
+    s << (ss ? ss : detail::fail(s));
+  }
+  else {
+    typedef typename detail::int_type<T>::type text_rep;
+      
+    s << text_rep(v[0]);
+    for (std::size_t i = 1; i < size; ++i)
+      s << " " << text_rep(v[i]);
   }
   
-  template<typename T, std::size_t size, typename InStream>
-  InStream &operator >>(InStream &s,
-                        const detail::color_reader_impl<T, size> &reader) {
-    return reader.read(s);
+  return s;  
+}  
+
+/**
+ * Read a generic vector from a stream. Also supports
+ * special values 'null'/'zero' for a vector with 0 length.
+ */
+template<typename T, std::size_t size, typename InStream>
+InStream &operator >>(InStream &s, vec<T, size> &v) {
+  const detail::streamstate<InStream> start(s);
+  if (!start)
+    return s;
+    
+  if (s >> v.components[0]) {
+    for (std::size_t i = 1; i < size && s.good(); ++i) {
+      typename detail::int_type<T>::type tmp;
+      s >> tmp;
+      v[i] = tmp;
+    }
   }
+  else {
+    // it seems we can't parse it as a native type of the vector,
+    // so we parse it as a string.
+    start.reset(s);
+    std::string textual;
+      
+    if (s >> textual) {
+      using detail::stricmp;
+      if (stricmp(textual.c_str(), "null") ||
+          stricmp(textual.c_str(), "zero")) {
+        for (std::size_t i = 0; i < size; ++i)
+          v[i] = T();
+      }
+      else {
+        s.clear(std::ios::failbit);
+      }
+    }
+  }
+  
+  return s;
+}
+  
+template<typename T, std::size_t size, typename InStream>
+InStream &operator >>(InStream &s,
+                      const detail::color_reader_impl<T, size> &reader) {
+  return reader.read(s);
+}
 
 } // !p
 
